d853eff221b16b9e7c997e24fea5e87a8cc063bd
Karine <karine.gevorgyan.2001@mail.ru>
2020-01-22 02:03:50 +0400

    Moved initializeStudentArrayList method to StudentService class

--- src/studentlists/Main.java
+++ src/studentlists/Main.java
@@ -15,17 +15,5 @@ public class Main {
         printStudentShortInfo(studentArrayList);
     }
 
-    public static ArrayList<Student> initializeStudentArrayList() {
-        ArrayList<Student> studentArrayList = new ArrayList<>();
-        studentArrayList.add(new Student("Emma", "Sharp", 25));
-        studentArrayList.add(new Student("Anthony", "Evans", 18));
-        studentArrayList.add(new Student("Rosie", "Bell", 17));
-        studentArrayList.add(new Student("Harvey", "Drew", 23));
-        studentArrayList.add(new Student("Emma", "Mccarthy", 37));
-        studentArrayList.add(new Student("Cole", "Cooper", 20));
-        studentArrayList.add(new Student("Isabel", "Griffin", 29));
-        studentArrayList.add(new Student("Cole", "Shelton", 31));
-        studentArrayList.add(new Student("Tommy", "Rodriguez", 16));
-        return studentArrayList;
-    }
+
 }
--- src/studentlists/StudentService.java
+++ src/studentlists/StudentService.java
@@ -1,5 +1,8 @@
 package studentlists;
 
+import studentlists.customformations.StudentDynamicArray;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,4 +15,20 @@ public class StudentService {
             System.out.println(student.getFirstName() + " " + student.getLastName() + " " + student.getAge());
         }
     }
+
+    public static ArrayList<Student> initializeStudentArrayList() {
+        ArrayList<Student> studentArrayList = new ArrayList<>();
+        studentArrayList.add(new Student("Emma", "Sharp", 25));
+        studentArrayList.add(new Student("Anthony", "Evans", 18));
+        studentArrayList.add(new Student("Rosie", "Bell", 17));
+        studentArrayList.add(new Student("Harvey", "Drew", 23));
+        studentArrayList.add(new Student("Emma", "Mccarthy", 37));
+        studentArrayList.add(new Student("Cole", "Cooper", 20));
+        studentArrayList.add(new Student("Isabel", "Griffin", 29));
+        studentArrayList.add(new Student("Cole", "Shelton", 31));
+        studentArrayList.add(new Student("Tommy", "Rodriguez", 16));
+        return studentArrayList;
+    }
+
+
 }
