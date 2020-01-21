075720f242317cd1edf26b4c910bbc05f1450e4b
Karine <karine.gevorgyan.2001@mail.ru>
2020-01-22 03:50:45 +0400

    Initiliazed a dynamic array with the custom initializer and tried to remove an element from it

--- src/studentlists/Main.java
+++ src/studentlists/Main.java
@@ -1,18 +1,30 @@
 package studentlists;
 
 import studentlists.comparators.*;
+import studentlists.customformations.CustomInitializer;
+import studentlists.customformations.StudentDynamicArray;
+
 import static studentlists.StudentService.*;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Student> studentArrayList = initializeStudentArrayList();
+        ArrayList<Student> studentArrayList = initializeStudents();
         printStudentShortInfo(studentArrayList);
         Collections.sort(studentArrayList, new StudentSorterByFirstName().thenComparing(new StudentSorterByAge().reversed()));
         System.out.println("--------------------Sorted by name(asc) and then by age(desc)-----------------------------");
         printStudentShortInfo(studentArrayList);
+        System.out.println("--------------------Dynamic array------------------");
+        CustomInitializer.initializeStudentDynamicArray();
+        StudentDynamicArray array = CustomInitializer.getDynamicArray();
+        printStudentShortInfo(array);
+        Student student = StudentService.findStudent(array, "Harvey", "Drew");
+        array.remove(student);
+        System.out.println("------After removing Harvey Drew--------");
+        printStudentShortInfo(array);
     }
 
 
