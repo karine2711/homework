039aaf373af254eb1215ac3df862894c7d6d4f7c
Karine <karine.gevorgyan.2001@mail.ru>
2020-01-22 03:48:44 +0400

    Implemented class StudentDynamicArray containing all the methods described in the requirements + private method ensure capacity

---
+++ src/studentlists/customformations/StudentDynamicArray.java
@@ -0,0 +1,62 @@
+package studentlists.customformations;
+
+import studentlists.Student;
+
+public class StudentDynamicArray {
+
+    private int size = 0;
+    private int position = -1;
+    private int capacity = 10;
+    private Student[] students = new Student[capacity];
+
+    public boolean add(Student student) {
+        position++;
+        if (position >= capacity) {
+            students = ensureCapacity(capacity * 2);
+        }
+        students[position] = student;
+
+        size++;
+        return true;
+    }
+
+    public boolean remove(Student s) {
+        int index = 0;
+        while (s != students[index] && index < size) {
+            index++;
+        }
+        if (index >= size) {
+            return false;
+        } else {
+
+            for (int i = index; i < position; i++) {
+                students[i] = students[i + 1];
+            }
+            students[position] = null;
+            size--;
+            position--;
+            return true;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Student get(int index) {
+        if (index <= position && index >= 0 && students[index] != null) {
+            return students[index];
+        } else {
+            return null;
+        }
+    }
+
+    private Student[] ensureCapacity(int capacity) {
+        Student temp[] = new Student[capacity];
+        for (int i = 0; i < capacity; i++) {
+            temp[i] = students[i];
+        }
+        return temp;
+    }
+
+}
