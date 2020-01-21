542be79306c6c3ebb84729bc7832452d8f07bc59
Karine <karine.gevorgyan.2001@mail.ru>
2020-01-22 00:20:52 +0400

    Incapsulated the Student class fields and provided a constructor

--- src/studentlists/Student.java
+++ src/studentlists/Student.java
@@ -1,8 +1,33 @@
 package studentlists;
 
 public class Student {
-    String firstName;
-    String lastName;
-    int age;
+    private String firstName;
+    private String lastName;
+    private int age;
 
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    //getters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    //setters
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
