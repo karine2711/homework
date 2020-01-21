deee641391d94218ec655973d5b19c1dbf2136a5
Karine <karine.gevorgyan.2001@mail.ru>
2020-01-22 01:35:27 +0400

    Added comparator classes which sort students by name and age respectively. In Main sorted the students by first name, if it's the same then by age in descending order

--- src/studentlists/Main.java
+++ src/studentlists/Main.java
@@ -10,6 +10,9 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Student> studentArrayList = initializeStudentArrayList();
         printStudentShortInfo(studentArrayList);
+        Collections.sort(studentArrayList, new StudentSorterByFirstName().thenComparing(new StudentSorterByAge().reversed()));
+        System.out.println("--------------------Sorted by name(asc) and then by age(desc)-----------------------------");
+        printStudentShortInfo(studentArrayList);
     }
 
     public static ArrayList<Student> initializeStudentArrayList() {
