625023f44bc1105bfe8f0b80fe9ede92323c2252
Karine <karine.gevorgyan.2001@mail.ru>
2020-01-23 02:14:48 +0400

    #Task2 Added printSortedList static method to Main and called it.

--- src/studentlists/Main.java
+++ src/studentlists/Main.java
@@ -11,25 +11,39 @@ import java.util.*;
 
 
 public class Main {
-    private static final String SEPERATOR="--------------------";
+    private static final String SEPERATOR = "--------------------";
 
     public static void main(String[] args) {
 
-        //task1
+        //task 1
+        System.out.println(SEPERATOR + "TASK 1" + SEPERATOR);
         List<Student> studentArrayList = initializeStudents();
-        printDoubleSortedList(studentArrayList,new StudentSorterByAge(), new StudentSorterByFirstName());
+        printDoubleSortedList(studentArrayList, new StudentSorterByFirstName(), new StudentSorterByAge());
 
+        //task 2
+        System.out.println(SEPERATOR + "TASK 2" + SEPERATOR);
+        List<Student> studentLinkedList = new LinkedList<>();
+        studentLinkedList.addAll(studentArrayList);
+        printSortedList(studentLinkedList);
 
     }
 
     public static void printDoubleSortedList(List list, Comparator comparator1,
-                                       Comparator comparator2){
-        System.out.println(SEPERATOR+"Initial List"+SEPERATOR);
+                                             Comparator comparator2) {
+        System.out.println(SEPERATOR + "Initial List" + SEPERATOR);
         printStudentShortInfo(list);
-        Collections.sort(list, new StudentSorterByFirstName().thenComparing(new StudentSorterByAge().reversed()));
-        System.out.println(SEPERATOR+"Sorted by name(asc) and then by age(desc)"+SEPERATOR);
+        Collections.sort(list, comparator1.thenComparing(comparator2.reversed()));
+        System.out.println(SEPERATOR + "Sorted by name(asc) and then by age(desc)" + SEPERATOR);
         printStudentShortInfo(list);
 
+    }
+
+    public static void printSortedList(List list) {
+        System.out.println(SEPERATOR + "Initial List" + SEPERATOR);
+        printStudentShortInfo(list);
+        Collections.sort(list);
+        System.out.println(SEPERATOR + "Sorted by last name" + SEPERATOR);
+        printStudentShortInfo(list);
     }
 
 }
