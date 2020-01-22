0ead39da06426551851e0d17c155490d256609ab
Karine <karine.gevorgyan.2001@mail.ru>
2020-01-23 01:37:06 +0400

    Removed testing calls and initializations from main. Added static printDoubleSortedList to Main and called it

--- src/studentlists/Main.java
+++ src/studentlists/Main.java
@@ -11,50 +11,25 @@ import java.util.*;
 
 
 public class Main {
+    private static final String SEPERATOR="--------------------";
+
     public static void main(String[] args) {
+
+        //task1
         List<Student> studentArrayList = initializeStudents();
-        printStudentShortInfo(studentArrayList);
-        Collections.sort(studentArrayList, new StudentSorterByFirstName().thenComparing(new StudentSorterByAge().reversed()));
-        System.out.println("--------------------Sorted by name(asc) and then by age(desc)-----------------------------");
-        printStudentShortInfo(studentArrayList);
-        System.out.println("--------------------Dynamic array------------------");
-        StudentDynamicArray array = CustomInitializer.getDynamicArray();
-        CustomInitializer.printStudentDynamicArray();
-        Student Harvey = StudentService.findStudent(array, "Harvey", "Drew");
-        array.remove(Harvey);
-        System.out.println("------After removing Harvey Drew--------");
-        CustomInitializer.printStudentDynamicArray();
+        printDoubleSortedList(studentArrayList,new StudentSorterByAge(), new StudentSorterByFirstName());
 
-        System.out.println("\n------Linked list sorted by last name--------");
-        LinkedList <Student> linkedList1=new LinkedList<Student>(studentArrayList);
-        Collections.sort(linkedList1);
-        for(Student student:linkedList1){
-            System.out.println(student.getLastName()+" "+student.getFirstName());
-        }
-
-        //make a linked list
-        StudentLinkedList linkedList = new StudentLinkedList();
-        linkedList.push(array.get(0));
-        linkedList.addLast(array.get(1));
-        System.out.println(linkedList.size());
-        linkedList.push(array.get(2));
-        linkedList.addLast(array.get(3));
-        linkedList.addLast(array.get(4));
-        System.out.println(linkedList.size());
-        Iterator<Student> iterator = linkedList.iterator();
-
-
-        System.out.println("------------");
-        System.out.println(linkedList.pop().getFirstName());
-        System.out.println(linkedList.removeLast().getFirstName());
-        System.out.println(linkedList.size());
-        System.out.println(linkedList.removeLast().getFirstName());
-        System.out.println(linkedList.pop().getFirstName());
-        System.out.println(linkedList.size());
-        System.out.println(linkedList.removeLast().getFirstName());
-        System.out.println(linkedList.size());
 
     }
 
+    public static void printDoubleSortedList(List list, Comparator comparator1,
+                                       Comparator comparator2){
+        System.out.println(SEPERATOR+"Initial List"+SEPERATOR);
+        printStudentShortInfo(list);
+        Collections.sort(list, new StudentSorterByFirstName().thenComparing(new StudentSorterByAge().reversed()));
+        System.out.println(SEPERATOR+"Sorted by name(asc) and then by age(desc)"+SEPERATOR);
+        printStudentShortInfo(list);
+
+    }
 
 }
