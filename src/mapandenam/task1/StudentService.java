79a9937fdbef6932502f4365f878e22770ff300d
Karine <karine.gevorgyan.2001@mail.ru>
2020-01-31 00:27:54 +0400

    Surrounded each operation of creating a new student and adding him/her to the list to be returned with a try/catch block

--- src/mapandenam/task1/StudentService.java
+++ src/mapandenam/task1/StudentService.java
@@ -1,4 +1,6 @@
-package mapandenam;
+package mapandenam.task1;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,33 +13,60 @@ public class StudentService {
     public static List<Student> initStudentList() {
         List<Student> students = new ArrayList<>();
 
-        //create some students
-        Student student1 = new Student("Diana", "Asatryan", 22,
-                "098811111", Faculty.MATH);
-        Student student2 = new Student("Diana", "Asatryan", 32,
-                "0922111111", Faculty.PHYSICS);
-        Student student3 = new Student("Diana", "Asatryan", 21,
-                "0938111111", Faculty.PHILOSOPHY);
-        Student student4 = new Student("Karen", "Balayan", 12,
-                "097777777", Faculty.MATH);
-        Student student5 = new Student("Karen", "Balayan", 23,
-                "098888811", Faculty.PHILOSOPHY);
-        Student student6 = new Student("Elen", "Mirzoyan", 12,
-                "093333333", Faculty.ENGLISH);
+        //create some students and add them to the list if all arguments are legal
+        try {
+            Student student1 = new Student("Diana", "Asatryan", 22,
+                    "098811111", Faculty.MATH);
+            students.add(student1);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
 
-        //add students to array list
-        students.add(student1);
-        students.add(student2);
-        students.add(student3);
-        students.add(student4);
-        students.add(student5);
-        students.add(student6);
+        try {
+            Student student2 = new Student("Diana", "Asatryan", 32,
+                    "0922111111", Faculty.PHYSICS);
+            students.add(student2);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Student student3 = new Student("Diana", "Asatryan", 21,
+                    "0938111111", Faculty.PHILOSOPHY);
+            students.add(student3);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Student student4 = new Student("Karen", "Balayan", 12,
+                    "097777777", Faculty.MATH);
+            students.add(student4);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Student student5 = new Student("Karen", "Balayan", 23,
+                    "098888811", Faculty.PHILOSOPHY);
+            students.add(student5);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Student student6 = new Student("Elen", "Mirzoyan", 12,
+                    "093333333", Faculty.ENGLISH);
+            students.add(student6);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
 
         return students;
     }
 
     //Returns a map of students with the number of occurrences of the same first and last names
-    public static Map<String, Integer> getStudentsMap(List<Student> students) {
+    public static Map<String, Integer> getStudentsMap(@NotNull List<Student> students) {
         Map<String, Integer> studentMap = new HashMap<>();
         for (Student student : students) {
             if (!studentMap.containsKey(student.getFullName())) {
@@ -52,11 +81,11 @@ public class StudentService {
     }
 
     //Counts the number of students in each faculty and stores this info in Faculty enumeration
-    public static void countStudentsInEachFaculty(List<Student> students) {
+    public static void countStudentsInEachFaculty(@NotNull List<Student> students) {
+        Faculty.resetStudentQuantity();
         for (Student student : students) {
             student.getFaculty().incrementStudentQuantity();
         }
     }
-
 
 }
