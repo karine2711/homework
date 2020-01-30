3f67207c217e69e018f045e1d30b77bd18ff8834
Karine <karine.gevorgyan.2001@mail.ru>
2020-01-30 21:33:43 +0400

    Created Student class along with its constructor and overriden equals(), hashCode() and toString() methods.

---
+++ src/mapandenam/Student.java
@@ -0,0 +1,49 @@
+package mapandenam;
+
+import java.util.Objects;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String phoneNumber;
+    private String faculty;
+
+    public Student(String firstName, String lastName, int age, String phoneNumber, String faculty) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.faculty = faculty;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                firstName.equals(student.firstName) &&
+                lastName.equals(student.lastName) &&
+                phoneNumber.equals(student.phoneNumber) &&
+                faculty.equals(student.faculty);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", faculty='" + faculty + '\'' +
+                '}';
+    }
+
+}
