f1236114799b6aa49d107bc76e6c4e50e3bf3dbc
Karine <karine.gevorgyan.2001@mail.ru>
2020-02-03 22:05:35 +0400

    Replaced multiple try/catch blocks in StudentService with a single method

--- src/mapandenum/task1/service/StudentService.java
+++ src/mapandenum/task1/service/StudentService.java
@@ -15,57 +15,33 @@ public class StudentService {
 
         List<Student> students = new ArrayList<>();
 
-        //create some students and add them to the list if all arguments are legal
-        try {
-            Student student1 = new Student("Diana", "Asatryan", 22,
-                    "098811111", Faculty.MATH);
-            students.add(student1);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Student student2 = new Student("Diana", "Asatryan", 32,
-                    "0922111111", Faculty.PHYSICS);
-            students.add(student2);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Student student3 = new Student("Diana", "Asatryan", 21,
-                    "0938111111", Faculty.PHILOSOPHY);
-            students.add(student3);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Student student4 = new Student("Karen", "Balayan", 12,
-                    "097777777", Faculty.MATH);
-            students.add(student4);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Student student5 = new Student("Karen", "Balayan", 23,
-                    "098888811", Faculty.PHILOSOPHY);
-            students.add(student5);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Student student6 = new Student("Elen", "Mirzoyan", 12,
-                    "093333333", Faculty.ENGLISH);
-            students.add(student6);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
+        // add some students to the list
+        addStudentInList("Diana", "Asatryan", 22,
+                "098811111", Faculty.MATH, students);
+        addStudentInList("Diana", "Asatryan", 32,
+                "0922111111", Faculty.PHYSICS, students);
+        addStudentInList("Diana", "Asatryan", 21,
+                "0938111111", Faculty.PHILOSOPHY, students);
+        addStudentInList("Karen", "Balayan", 12,
+                "097777777", Faculty.MATH, students);
+        addStudentInList("Karen", "Balayan", 23,
+                "098888811", Faculty.PHILOSOPHY, students);
+        addStudentInList("Elen", "Mirzoyan", 12,
+                "093333333", Faculty.ENGLISH, students);
 
         return students;
 
+    }
+
+    private static void addStudentInList(String name, String surname, int age, String phoneNumber,
+                                         Faculty faculty, List<Student> students) {
+        try {
+            Student student = new Student(name, surname, age,
+                    phoneNumber, faculty);
+            students.add(student);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
 
     //Returns a map of students with the number of occurrences of the same first and last names
@@ -94,5 +70,6 @@ public class StudentService {
         }
 
     }
+
 
 }
