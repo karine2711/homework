242225b8ce720bcfb7216f032e0b0a168032974a
Karine <karine.gevorgyan.2001@mail.ru>
2020-02-04 01:29:05 +0400

    Wrote an iterator for CustomTreeSet

--- src/setsAndGenerics/CustomTreeSet.java
+++ src/setsAndGenerics/CustomTreeSet.java
@@ -1,44 +1,93 @@
 package setsAndGenerics;
 
-public class CustomTreeSet<Type extends Comparable<Type>> {
- Node root=null;
-    private class Node{
+import java.util.Iterator;
+
+public class CustomTreeSet<Type extends Comparable<Type>> implements Iterable<Type> {
+    Node root = null;
+
+    @Override
+    public Iterator<Type> iterator() {
+        Node LeftEndLeaf = traverseLeft();
+        return new CustomTreeSetIterator(LeftEndLeaf);
+    }
+
+    private class Node {
         Node parent;
         Node left;
         Node right;
-       Type current;
+        Type current;
 
-        public Node(Type t, Node left, Node right,Node parent ) {
+        public Node(Type t, Node left, Node right, Node parent) {
             this.left = left;
             this.right = right;
             this.current = t;
-            this.parent=parent;
+            this.parent = parent;
         }
     }
 
-    public void add(Type object){
-        if(root==null){
-            root=new Node(object,null,null,null);
-        }else {findPlace(object,root);}
+    public void add(Type object) {
+        if (root == null) {
+            root = new Node(object, null, null, null);
+        } else {
+            findPlace(object, root);
+        }
     }
 
-    private void findPlace(Type object,Node node){
-        if(object.compareTo(node.current)<0){
-            if(node.left==null){
-               node.left=new Node(object,null,null,node);
-            }else {
-                 findPlace(object,node.left);
+    private void findPlace(Type object, Node node) {
+        if (object.compareTo(node.current) < 0) {
+            if (node.left == null) {
+                node.left = new Node(object, null, null, node);
+            } else {
+                findPlace(object, node.left);
             }
-        }else if (object.compareTo(node.current)>0){
-            if(node.right==null){
-                node.right=new Node(object,null,null,node);
-            }else {
-                 findPlace(object,node.right);
+        } else if (object.compareTo(node.current) > 0) {
+            if (node.right == null) {
+                node.right = new Node(object, null, null, node);
+            } else {
+                findPlace(object, node.right);
             }
         }
     }
 
-//    public boolean contains(Type object){
-//
-//    }
+    private Node traverseLeft() {
+        Node position = root;
+        while (position.left != null) {
+            position = position.left;
+        }
+        return position;
+    }
+
+    private class CustomTreeSetIterator implements Iterator {
+        Node position;
+        boolean rightReturned = false;
+
+        public CustomTreeSetIterator(Node leftEndLeaf) {
+            position =new Node(null,null,null,leftEndLeaf) ;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (position.right != null || position.parent != null) {
+                return true;
+            }
+            return false;
+
+        }
+
+        @Override
+        public Type next() {
+            if (this.hasNext()) {
+                if (position.right != null && !rightReturned) {
+                    rightReturned = true;
+                    return position.right.current;
+                } else {
+                    position = position.parent;
+                    rightReturned = false;
+                    return position.current;
+                }
+            }
+            return null;
+        }
+    }
+
 }
--- src/setsAndGenerics/Main.java
+++ src/setsAndGenerics/Main.java
@@ -1,12 +1,21 @@
 package setsAndGenerics;
 
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
         CustomTreeSet<String> set=new CustomTreeSet<String>();
-        set.add("hi");
-        set.add("no");
-        set.add("yes");
-        set.add("aamm");
-        System.out.println("hi");
+        set.add("5");
+        set.add("2");
+        set.add("3");
+        set.add("1");
+
+     //   System.out.println("hi");
+        Iterator<String> iterator=set.iterator();
+        for (String s:set
+             ) {
+            System.out.println(s);
+
+        }
     }
 }
