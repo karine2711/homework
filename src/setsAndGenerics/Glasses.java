239bef5d13fa4ccb9d5725c7ba9ff0b8fd01003e
Karine <karine.gevorgyan.2001@mail.ru>
2020-02-05 20:41:58 +0400

    Created Glasses class, than its set in Main and tested"

---
+++ src/setsAndGenerics/Glasses.java
@@ -0,0 +1,94 @@
+package setsAndGenerics;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Glasses implements Comparable<Glasses> {
+
+    private String brand;
+    private boolean isOptical;
+    private String bordercolor;
+    private Glass[] glasses = new Glass[2];
+
+
+    public Glasses(String brand, String bordercolor, String leftGlassColor, String rightGlassColor,
+                   double rightOpticalRange, double leftOpticalRange) {
+        this.brand = brand;
+        this.bordercolor = bordercolor;
+        Glass left = new Glass();
+        left.opticalRange = leftOpticalRange;
+        left.color = leftGlassColor;
+
+        Glass right = new Glass();
+        right.opticalRange = rightOpticalRange;
+        right.color = rightGlassColor;
+
+        if (rightOpticalRange != 0 || leftOpticalRange != 0) {
+            isOptical = true;
+        }
+        glasses[0] = left;
+        glasses[1] = right;
+
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public boolean getIsOptical() {
+        return this.isOptical;
+    }
+
+    public String getBordercolor() {
+        return this.bordercolor;
+    }
+
+    public Glass[] getGlasses() {
+        return this.glasses;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        final Glasses glasses1 = (Glasses) o;
+        return this.isOptical == glasses1.isOptical &&
+                this.brand.equals(glasses1.brand) &&
+                this.bordercolor.equals(glasses1.bordercolor) &&
+                Arrays.equals(this.glasses, glasses1.glasses);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(this.brand, this.isOptical, this.bordercolor);
+        result = 31 * result + Arrays.hashCode(this.glasses);
+        return result;
+    }
+
+    @Override
+    public int compareTo( Glasses glasses) {
+        return this.hashCode()-glasses.hashCode();
+    }
+
+    private class Glass {
+        String color = "transparent";
+        double opticalRange;
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) return true;
+            if (o == null || this.getClass() != o.getClass()) return false;
+            final Glass glass = (Glass) o;
+            return Double.compare(glass.opticalRange, this.opticalRange) == 0 &&
+                    Objects.equals(this.color, glass.color);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.color, this.opticalRange);
+        }
+    }
+
+}
--- src/setsAndGenerics/Main.java
+++ src/setsAndGenerics/Main.java
@@ -5,27 +5,28 @@ import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-      testStringCustomSet();
-      testCoffeeCustomSet();
-      testStudentCustomSet();
+        testStringCustomSet();
+        testCoffeeCustomSet();
+        testStudentCustomSet();
+        testGlassesSet();
     }
 
 
-    public static void testStringCustomSet() {
+    private static void testStringCustomSet() {
         CustomTreeSet<String> set = new CustomTreeSet<String>();
 
-        System.out.println("Add '1': "+set.add("1"));
-        System.out.println("Add '5': "+set.add("5"));
-        System.out.println("Add '3': "+set.add("3"));
-        System.out.println("Add '3': "+set.add("3"));
+        System.out.println("Add '1': " + set.add("1"));
+        System.out.println("Add '5': " + set.add("5"));
+        System.out.println("Add '3': " + set.add("3"));
+        System.out.println("Add '3': " + set.add("3"));
 
-        System.out.println("Size = "+set.getSize());
-        System.out.println("Remove '5': "+set.remove("5"));
-        System.out.println("Size = "+set.getSize());
-        System.out.println("Contains '5': "+set.contains("5"));
+        System.out.println("Size = " + set.getSize());
+        System.out.println("Remove '5': " + set.remove("5"));
+        System.out.println("Size = " + set.getSize());
+        System.out.println("Contains '5': " + set.contains("5"));
     }
 
-    public static void testCoffeeCustomSet() {
+    private static void testCoffeeCustomSet() {
         //Make cappuccion
         Coffee cappuccino = new Coffee.CoffeeMachine("Cappuccino", 350)
                 .addSugar(2)
@@ -52,37 +53,57 @@ public class Main {
         //Create a coffee set
         CustomTreeSet<Coffee> coffeeSet = new CustomTreeSet<>();
 
-        System.out.println("Cappuccino added: "+coffeeSet.add(cappuccino)); //expected true
-        System.out.println("Latte added: "+coffeeSet.add(latte)); //expected true
+        System.out.println("Cappuccino added: " + coffeeSet.add(cappuccino)); //expected true
+        System.out.println("Latte added: " + coffeeSet.add(latte)); //expected true
 
         //expected false as a cappuccino with an exactly same recipe exists in the set
-        System.out.println("Cappuccino duplicate added: "+coffeeSet.add(cappuccinoCopy));
+        System.out.println("Cappuccino duplicate added: " + coffeeSet.add(cappuccinoCopy));
 
-        System.out.println("New cappuccino added: "+coffeeSet.add(cappuccino2)); //expected true
-        System.out.println("2nd Cappuccino removed: "+coffeeSet.remove(cappuccino2)); //expected true
-        System.out.println("Cappuccino removed: "+coffeeSet.remove(cappuccino)); //expected true
+        System.out.println("New cappuccino added: " + coffeeSet.add(cappuccino2)); //expected true
+        System.out.println("2nd Cappuccino removed: " + coffeeSet.remove(cappuccino2)); //expected true
+        System.out.println("Cappuccino removed: " + coffeeSet.remove(cappuccino)); //expected true
 
         //expected false, as cappuccino was just removed
         System.out.println("Check if there is a cappchino with a given recipe in the set: "
-                +coffeeSet.contains(cappuccino));
+                + coffeeSet.contains(cappuccino));
 
-        System.out.println("Size = "+coffeeSet.getSize());
+        System.out.println("Size = " + coffeeSet.getSize());
     }
 
-    public static void testStudentCustomSet(){
-        Student karine=new Student("Karine", "Gevorgyan",18);
-        Student milena=new Student("Milena", "Xazaryan", 20);
-        Student babken=new Student("Babken", "Pejpejyan", 19);
+    private static void testStudentCustomSet() {
+        Student karine = new Student("Karine", "Gevorgyan", 18);
+        Student milena = new Student("Milena", "Xazaryan", 20);
+        Student babken = new Student("Babken", "Pejpejyan", 19);
 
-        CustomTreeSet<Student> studentSet=new CustomTreeSet<>();
-        System.out.println("Add Karine: "+studentSet.add(karine));
-        System.out.println("Add Milena: "+ studentSet.add(milena));
-        System.out.println("Add Babken: "+ studentSet.add(babken));
-        System.out.println("Add Milena again: "+ studentSet.add(milena));
-        System.out.println("Size = "+studentSet.getSize());
+        CustomTreeSet<Student> studentSet = new CustomTreeSet<>();
+        System.out.println("Add Karine: " + studentSet.add(karine));
+        System.out.println("Add Milena: " + studentSet.add(milena));
+        System.out.println("Add Babken: " + studentSet.add(babken));
+        System.out.println("Add Milena again: " + studentSet.add(milena));
+        System.out.println("Size = " + studentSet.getSize());
 
-        System.out.println("Remove Karine: "+studentSet.remove(karine));
-        System.out.println("Contains Karine? "+studentSet.contains(karine));
-        System.out.println("Contains Miilena? "+studentSet.contains(milena));
+        System.out.println("Remove Karine: " + studentSet.remove(karine));
+        System.out.println("Contains Karine? " + studentSet.contains(karine));
+        System.out.println("Contains Miilena? " + studentSet.contains(milena));
+    }
+
+    private static void testGlassesSet(){
+        Glasses raybans = new Glasses("Ray-Ban", "blue", "black",
+                "black", +3, +3.5);
+        Glasses prada = new Glasses("Prada", "pink", "black",
+                "black", 0, 0);
+        Glasses tomFord = new Glasses("Tom Ford", "black", "yellow",
+                "yellow", 0, 0);
+        Glasses prada2 = new Glasses("Prada", "pink", "black",
+                "black", 0, 0);
+        CustomTreeSet<Glasses> glassesSet = new CustomTreeSet<>();
+        System.out.println("Add Rayb-Ban: "+glassesSet.add(raybans));
+        System.out.println("Add Prada: "+glassesSet.add(prada));
+        System.out.println("Add Tom-Ford: "+glassesSet.add(tomFord));
+        System.out.println("Add Prada duplicate: "+glassesSet.add(prada2));
+        System.out.println("Size: "+glassesSet.getSize());
+        System.out.println("Contains Raybans?: "+glassesSet.contains(raybans));
+        System.out.println("Remove Raybans: "+glassesSet.remove(raybans));
+        System.out.println("Contains Raybans?: "+glassesSet.contains(raybans));
     }
 }
