6b94a5fb2aac1cb86a256d8f49d80e7d13ac9e95
Karine <karine.gevorgyan.2001@mail.ru>
2020-02-05 22:42:00 +0400

    Called pritAll() in all setTester static functions in Main

--- src/setsAndGenerics/Main.java
+++ src/setsAndGenerics/Main.java
@@ -9,40 +9,51 @@ public class Main {
         testCoffeeCustomSet();
         testStudentCustomSet();
         testGlassesSet();
+
     }
 
 
     private static void testStringCustomSet() {
+        //create a  set
         CustomTreeSet<String> set = new CustomTreeSet<String>();
 
+        //add some String objects
         System.out.println("Add '1': " + set.add("1"));
         System.out.println("Add '5': " + set.add("5"));
         System.out.println("Add '3': " + set.add("3"));
         System.out.println("Add '3': " + set.add("3"));
 
+        set.printAll();
+
+        //Test the methods
         System.out.println("Size = " + set.getSize());
         System.out.println("Remove '5': " + set.remove("5"));
         System.out.println("Size = " + set.getSize());
         System.out.println("Contains '5': " + set.contains("5"));
+
     }
 
     private static void testCoffeeCustomSet() {
+
         //Make cappuccion
         Coffee cappuccino = new Coffee.CoffeeMachine("Cappuccino", 350)
                 .addSugar(2)
                 .addMilk(50)
                 .make();
+
         //Make cappuccino duplicate
         Coffee cappuccinoCopy = new Coffee.CoffeeMachine("Cappuccino", 350)
                 .addSugar(2)
                 .addMilk(50)
                 .make();
+
         //Make latte
         Coffee latte = new Coffee.CoffeeMachine("Esspresso", 250)
                 .addMilk(100)
                 .addIceCream(10)
                 .addSugar(1)
                 .make();
+
         //Make another cappuccino, different from the initial one
         Coffee cappuccino2 = new Coffee.CoffeeMachine("Cappuccino", 350)
                 .addSugar(5)
@@ -53,26 +64,33 @@ public class Main {
         //Create a coffee set
         CustomTreeSet<Coffee> coffeeSet = new CustomTreeSet<>();
 
-        System.out.println("Cappuccino added: " + coffeeSet.add(cappuccino)); //expected true
-        System.out.println("Latte added: " + coffeeSet.add(latte)); //expected true
+        //Add coffeees to set
+        System.out.println("Cappuccino added: " + coffeeSet.add(cappuccino));
+        System.out.println("Latte added: " + coffeeSet.add(latte));
 
         //expected false as a cappuccino with an exactly same recipe exists in the set
         System.out.println("Cappuccino duplicate added: " + coffeeSet.add(cappuccinoCopy));
 
-        System.out.println("New cappuccino added: " + coffeeSet.add(cappuccino2)); //expected true
-        System.out.println("2nd Cappuccino removed: " + coffeeSet.remove(cappuccino2)); //expected true
-        System.out.println("Cappuccino removed: " + coffeeSet.remove(cappuccino)); //expected true
+        System.out.println("New cappuccino added: " + coffeeSet.add(cappuccino2));
+
+        coffeeSet.printAll();
+
+        System.out.println("2nd Cappuccino removed: " + coffeeSet.remove(cappuccino2));
+        System.out.println("Cappuccino removed: " + coffeeSet.remove(cappuccino));
 
         //expected false, as cappuccino was just removed
         System.out.println("Check if there is a cappchino with a given recipe in the set: "
                 + coffeeSet.contains(cappuccino));
 
         System.out.println("Size = " + coffeeSet.getSize());
+
+
     }
+
 
     private static void testStudentCustomSet() {
         Student karine = new Student("Karine", "Gevorgyan", 18);
-        Student milena = new Student("Milena", "Xazaryan", 20);
+        Student milena = new Student("Milena", "Xazaryan", 18);
         Student babken = new Student("Babken", "Pejpejyan", 19);
 
         CustomTreeSet<Student> studentSet = new CustomTreeSet<>();
@@ -82,12 +100,16 @@ public class Main {
         System.out.println("Add Milena again: " + studentSet.add(milena));
         System.out.println("Size = " + studentSet.getSize());
 
+        studentSet.printAll();
+
         System.out.println("Remove Karine: " + studentSet.remove(karine));
         System.out.println("Contains Karine? " + studentSet.contains(karine));
         System.out.println("Contains Miilena? " + studentSet.contains(milena));
     }
 
-    private static void testGlassesSet(){
+    private static void testGlassesSet() {
+
+        //Created some Glasses
         Glasses raybans = new Glasses("Ray-Ban", "blue", "black",
                 "black", +3, +3.5);
         Glasses prada = new Glasses("Prada", "pink", "black",
@@ -96,14 +118,17 @@ public class Main {
                 "yellow", 0, 0);
         Glasses prada2 = new Glasses("Prada", "pink", "black",
                 "black", 0, 0);
-        CustomTreeSet<Glasses> glassesSet = new CustomTreeSet<>();
-        System.out.println("Add Rayb-Ban: "+glassesSet.add(raybans));
-        System.out.println("Add Prada: "+glassesSet.add(prada));
-        System.out.println("Add Tom-Ford: "+glassesSet.add(tomFord));
-        System.out.println("Add Prada duplicate: "+glassesSet.add(prada2));
-        System.out.println("Size: "+glassesSet.getSize());
-        System.out.println("Contains Raybans?: "+glassesSet.contains(raybans));
-        System.out.println("Remove Raybans: "+glassesSet.remove(raybans));
-        System.out.println("Contains Raybans?: "+glassesSet.contains(raybans));
+
+        CustomTreeSet<Glasses> glassesSet = new CustomTreeSet<>(); //create a set of Glasses
+
+        //Test the methods
+        System.out.println("Add Rayb-Ban: " + glassesSet.add(raybans));
+        System.out.println("Add Prada: " + glassesSet.add(prada));
+        System.out.println("Add Tom-Ford: " + glassesSet.add(tomFord));
+        System.out.println("Add Prada duplicate: " + glassesSet.add(prada2));
+        System.out.println("Size: " + glassesSet.getSize());
+        System.out.println("Contains Raybans?: " + glassesSet.contains(raybans));
+        System.out.println("Remove Raybans: " + glassesSet.remove(raybans));
+        System.out.println("Contains Raybans?: " + glassesSet.contains(raybans));
     }
 }
