31c89dfd132d9b5bade625dc46cefc98893ba5ec
Karine <karine.gevorgyan.2001@mail.ru>
2020-02-11 21:53:40 +0400

    Added a counter to Main object, so that I can control the order of threads in method in print

--- src/threads/Main.java
+++ src/threads/Main.java
@@ -1,6 +1,8 @@
 package threads;
 
 public class Main {
+    int counter;
+
     public static void main(String[] args) {
         Main obj = new Main();
         MyThread karine = new MyThread("Karine", obj);
@@ -12,15 +14,25 @@ public class Main {
     }
 
     public synchronized void print() {
+        if ((Thread.currentThread().getName() == "Karine" && counter % 3 == 0) ||
+                (Thread.currentThread().getName() == "Marine" && counter % 3 == 1) ||
+                (Thread.currentThread().getName() == "Ani" && counter % 3 == 2)) {
             System.out.println(Thread.currentThread().getName());
             System.out.println(System.currentTimeMillis());
             notify();
+            counter++;
             try {
                 Thread.sleep(2000);
-                wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        try {
+            notify();
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+}
 
