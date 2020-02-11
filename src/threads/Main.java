9825c08a6a8068ad09811631f126379f44a01181
Karine <karine.gevorgyan.2001@mail.ru>
2020-02-11 21:42:30 +0400

    Created synchronized method print() in Main.java and called it in the run method of MyThread. Tested so far written code in psvm, however now I want the names to be printed in given order

--- src/threads/Main.java
+++ src/threads/Main.java
@@ -1,4 +1,26 @@
 package threads;
 
 public class Main {
-}
+    public static void main(String[] args) {
+        Main obj = new Main();
+        MyThread karine = new MyThread("Karine", obj);
+        MyThread marine = new MyThread("Marine", obj);
+        MyThread ani = new MyThread("Ani", obj);
+        karine.start();
+        marine.start();
+        ani.start();
+    }
+
+    public synchronized void print() {
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(System.currentTimeMillis());
+            notify();
+            try {
+                Thread.sleep(2000);
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
--- src/threads/MyThread.java
+++ src/threads/MyThread.java
@@ -10,6 +10,8 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
-
+        for(int i=0; i<10; i++){
+            resource.print();
+        }
     }
 }
